b66a366783adb002dedddd435c40cd5597736934
Aditya Jain - Developer Indore <adityan.jain@infobeans.com>
2021-04-29 17:45:59 +0530

    text styling

--- The Net Ninja/vuetify-tutorial/src/views/Home.vue
+++ The Net Ninja/vuetify-tutorial/src/views/Home.vue
@@ -1,5 +1,27 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <h1>Hello World</h1>
+    <p class="red white--text">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, dolores
+      laboriosam tempore id voluptatibus sit voluptatum incidunt nihil quo
+      laborum consectetur, molestiae reiciendis eveniet perferendis totam. Vitae
+      ipsa reiciendis doloribus?
+    </p>
+
+    <p class="pink lighten-4 red--text text--darken-4">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio,
+      voluptate! Magni nesciunt laudantium, labore optio ipsam doloribus! Non
+      eligendi architecto aspernatur perferendis placeat, voluptatum itaque!
+      Fugit voluptates id harum doloribus!
+    </p>
+
+    <h1 class="display-4">Massive display</h1>
+    <h4 class="display-1">small display</h4>
+    <h1>test</h1>
+    <p class="heading">heading</p>
+    <p class="subheading font-weight-bold">sub heading</p>
+    <p class="caption">this is caption</p>
+  </div>
 </template>
 
 <script>
