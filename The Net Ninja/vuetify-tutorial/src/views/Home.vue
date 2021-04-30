de3ea563511b001df323858915570570005560f2
Aditya Jain - Developer Indore <adityan.jain@infobeans.com>
2021-04-30 15:43:54 +0530

    breakpoints

--- The Net Ninja/vuetify-tutorial/src/views/Home.vue
+++ The Net Ninja/vuetify-tutorial/src/views/Home.vue
@@ -1,26 +1,11 @@
 <template>
   <div>
-    <h1>Hello World</h1>
-    <v-btn class="pink white--text">Click me</v-btn>
-    <v-btn dark depressed color="pink ">click me</v-btn>
-    <v-btn flat color="pink">click me</v-btn>
+    <h1>break points</h1>
+    <v-btn class="hidden-md-and-down">Click Me on md-and-down</v-btn>
 
-    <v-btn depressed class="pink white--text"
-      ><v-icon left>mdi-email-remove</v-icon> click me</v-btn
-    >
+    <v-btn class="hidden-md-and-up">click me on MD and up</v-btn>
 
-    <v-btn depressed small class="pink white--text"
-      ><v-icon small left>mdi-email-remove</v-icon> click me</v-btn
-    >
-
-    <v-btn depressed large class="pink white--text">
-      <span>click me</span>
-      <v-icon right large>mdi-email-remove</v-icon>
-    </v-btn>
-
-    <v-btn fab depressed small dark color="purple">
-      <v-icon>mdi-email-remove</v-icon>
-    </v-btn>
+    <v-btn class="hidden-sm-only">click me -sm-only</v-btn>
   </div>
 </template>
 
