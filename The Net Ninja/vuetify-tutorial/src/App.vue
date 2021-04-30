dba413bbea55a165a18a8bd8db0ae58d196ecb55
Aditya Jain - Developer Indore <adityan.jain@infobeans.com>
2021-04-30 19:58:17 +0530

    built login component with vuetify

--- The Net Ninja/vuetify-tutorial/src/App.vue
+++ The Net Ninja/vuetify-tutorial/src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <v-app>
+  <v-app class="grey">
+    <Navbar />
+    <Login />
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue';
+import Login from '@/components/Login.vue';
 
 export default {
   name: 'App',
-
+  components: { Navbar, Login },
   data: () => ({
+
     //
   }),
 };
---
+++ The Net Ninja/vuetify-tutorial/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+<nav>
+
+    <v-toolbar flat app>
+
+    </v-toolbar>
+</nav>
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
