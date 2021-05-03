81f2e70fefb299d2a26094eada8ef1c313a8161f
Aditya Jain - Developer Indore <adityan.jain@infobeans.com>
2021-05-03 19:38:44 +0530

    table & breakpoints

--- The Net Ninja/vuetify-tutorial/src/App.vue
+++ The Net Ninja/vuetify-tutorial/src/App.vue
@@ -2,6 +2,7 @@
   <v-app class="grey">
     <Navbar />
     <Login />
+    <Table />
     <v-main>
       <router-view />
     </v-main>
@@ -11,12 +12,12 @@
 <script>
 import Navbar from '@/components/Navbar.vue';
 import Login from '@/components/Login.vue';
+import Table from '@/components/Table.vue';
 
 export default {
   name: 'App',
-  components: { Navbar, Login },
+  components: { Navbar, Login, Table },
   data: () => ({
-
     //
   }),
 };
