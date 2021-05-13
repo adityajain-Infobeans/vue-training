79c906b42c38c995b3f5a13b2a8958eb3a94d377
Aditya Jain - Developer Indore <adityan.jain@infobeans.com>
2021-05-13 17:24:09 +0530

    mode table responsive

--- Jira Lite (Project)/front-end/src/components/Dashboard/TicketsTable.vue
+++ Jira Lite (Project)/front-end/src/components/Dashboard/TicketsTable.vue
@@ -2,60 +2,117 @@
   <v-container>
     <h2 class="display-2 mb-6 black--text">Tickets:</h2>
 
-    <!-- refer this  https://stackoverflow.com/questions/59081299/vuetify-insert-action-button-in-data-table-and-get-row-data -->
-    <v-data-table
-      :headers="this.$store.state.headers"
-      :items="this.$store.state.tickets"
-      :items-per-page="10"
-      class="elevation-1"
-      dark
-    >
-      <template v-slot:item="row">
-        <tr class="text-center">
-          <td>{{ row.item.ticket_id }}</td>
-          <td>{{ row.item.ticket_status }}</td>
-          <td class="font-weight-bold">
-            <v-chip :color="bgColor(row.item.ticket_priority)" light>
-              {{ row.item.ticket_priority }}
-            </v-chip>
-          </td>
-          <td
-            class="text-left font-weight-bold"
-            @click="detailTicket(row.item.ticket_id)"
-          >
-            {{
-              row.item.ticket_subject.length > 140
-                ? row.item.ticket_subject.substring(0, 140) + '...'
-                : row.item.ticket_subject
-            }}
-          </td>
-          <td>{{ row.item.ticket_last_updated }}</td>
-          <td>
-            <v-btn
-              class="blue white--text"
-              @click="editTicket(row.item.ticket_id)"
-              small
+    <v-layout v-resize="onResize">
+      <!-- refer this  https://stackoverflow.com/questions/59081299/vuetify-insert-action-button-in-data-table-and-get-row-data -->
+      <v-data-table
+        :headers="this.$store.state.headers"
+        :items="this.$store.state.tickets"
+        :items-per-page="10"
+        class="elevation-1"
+        dark
+        :hide-headers="isMobile"
+        :class="{ mobile: isMobile }"
+      >
+        <template v-slot:item="row">
+          <tr class="text-center" v-if="!isMobile">
+            <td>{{ row.item.ticket_id }}</td>
+            <td>{{ row.item.ticket_status }}</td>
+            <td class="font-weight-bold">
+              <v-chip :color="bgColor(row.item.ticket_priority)" light>
+                {{ row.item.ticket_priority }}
+              </v-chip>
+            </td>
+            <td
+              class="text-left font-weight-bold"
+              @click="detailTicket(row.item.ticket_id)"
             >
-              <v-icon>mdi-pencil</v-icon> Edit</v-btn
-            >
-          </td>
-          <td>
-            <v-btn
-              class="red white--text"
-              @click="deleteTicket(row.item.ticket_id)"
-              small
-            >
-              <v-icon>mdi-delete</v-icon> Delete</v-btn
-            >
-          </td>
-        </tr>
-      </template>
-    </v-data-table>
+              {{
+                row.item.ticket_subject.length > 140
+                  ? row.item.ticket_subject.substring(0, 140) + '...'
+                  : row.item.ticket_subject
+              }}
+            </td>
+            <td>{{ row.item.ticket_last_updated }}</td>
+            <td>
+              <v-btn
+                class="blue white--text"
+                @click="editTicket(row.item.ticket_id)"
+                small
+              >
+                <v-icon>mdi-pencil</v-icon> Edit</v-btn
+              >
+            </td>
+            <td>
+              <v-btn
+                class="red white--text"
+                @click="deleteTicket(row.item.ticket_id)"
+                small
+              >
+                <v-icon>mdi-delete</v-icon> Delete</v-btn
+              >
+            </td>
+          </tr>
+          <tr class="text-center" v-else>
+            <td>
+              <ul class="flex-content">
+                <li class="flex-item" data-label="Name">
+                  {{ row.item.ticket_id }}
+                </li>
+                <li class="flex-item" data-label="Name">
+                  {{ row.item.ticket_status }}
+                </li>
+                <li class="font-weight-bold flex-item" data-label="Name">
+                  <v-chip :color="bgColor(row.item.ticket_priority)" light>
+                    {{ row.item.ticket_priority }}
+                  </v-chip>
+                </li>
+                <li
+                  class="text-left font-weight-bold flex-item"
+                  data-label="Name"
+                  @click="detailTicket(row.item.ticket_id)"
+                >
+                  {{
+                    row.item.ticket_subject.length > 140
+                      ? row.item.ticket_subject.substring(0, 140) + '...'
+                      : row.item.ticket_subject
+                  }}
+                </li>
+                <li class="flex-item" data-label="Name">
+                  {{ row.item.ticket_last_updated }}
+                </li>
+                <li class="flex-item" data-label="Name">
+                  <v-btn
+                    class="blue white--text"
+                    @click="editTicket(row.item.ticket_id)"
+                    small
+                  >
+                    <v-icon>mdi-pencil</v-icon> Edit</v-btn
+                  >
+                </li>
+                <li class="flex-item" data-label="Name">
+                  <v-btn
+                    class="red white--text"
+                    @click="deleteTicket(row.item.ticket_id)"
+                    small
+                  >
+                    <v-icon>mdi-delete</v-icon> Delete</v-btn
+                  >
+                </li>
+              </ul>
+            </td>
+          </tr>
+        </template>
+      </v-data-table>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    isMobile: false,
+  }),
+
   methods: {
     editTicket(id) {
       this.$router.push({ path: `/updateticket/${id}` });
@@ -79,9 +136,26 @@ export default {
       }
       return 'green';
     },
+
+    onResize() {
+      if (window.innerWidth < 650) this.isMobile = true;
+      else this.isMobile = false;
+    },
   },
 };
 </script>
 
-<style>
+<style scoped>
+.flex-content {
+  padding: 1.5rem 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+.flex-item {
+  padding: 0.5rem;
+}
 </style>
