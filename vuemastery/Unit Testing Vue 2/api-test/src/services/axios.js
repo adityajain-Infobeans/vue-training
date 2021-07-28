bc631fd076f535bfdd1ff83d588fa2dc6acba7bc
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-07-28 19:25:53 +0530

    adding catching for falure

--- vuemastery/Unit Testing Vue 2/api-test/src/services/axios.js
+++ vuemastery/Unit Testing Vue 2/api-test/src/services/axios.js
@@ -1,6 +1,11 @@
+/* eslint-disable quotes */
+/* eslint-disable arrow-parens */
 /* eslint-disable import/prefer-default-export */
-import axios from 'axios';
+import axios from "axios";
 
 export function getMessage() {
-  return axios.get('http://localhost:3000/message').then((response) => response.data);
+  return axios
+    .get("http://localhost:3000/message")
+    .then(response => response.data)
+    .catch(error => error);
 }
