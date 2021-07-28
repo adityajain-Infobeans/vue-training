549b75c48dd92781b8f2ae09d5eca781b06980a1
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-07-28 19:21:06 +0530

    async async to promise

--- vuemastery/Unit Testing Vue 2/api-test/src/components/HelloWorld.vue
+++ vuemastery/Unit Testing Vue 2/api-test/src/components/HelloWorld.vue
@@ -1,3 +1,4 @@
+/* eslint-disable no-return-assign */ /* eslint-disable quotes */
 <template>
   <p v-if="error" data-testid="message-error">{{ error }}</p>
   <p v-else data-testid="message">{{ message.text }}</p>
@@ -14,12 +15,17 @@ export default {
     };
   },
 
-  async created() {
-    try {
-      this.message = await getMessage();
-    } catch (err) {
-      this.error = 'Oops! Something went wrong.';
-    }
+  created() {
+    getMessage()
+      // eslint-disable-next-line arrow-parens
+      // eslint-disable-next-line no-return-assign
+      .then((response) => (this.message = response))
+      // eslint-disable-next-line arrow-parens
+      // eslint-disable-next-line no-return-assign
+      .catch((error) => {
+        this.error = 'Oops! Something went wrong';
+        console.log(error);
+      });
   },
 };
 </script>
