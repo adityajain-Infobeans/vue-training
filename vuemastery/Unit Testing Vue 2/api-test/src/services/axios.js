0a41d394382b367c114d5d24645496bf97cddcf8
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-07-28 17:17:30 +0530

    api call function

---
+++ vuemastery/Unit Testing Vue 2/api-test/src/services/axios.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+import axios from 'axios';
+
+export function getMessage() {
+  return axios.get('http://localhost:3000/message').then((response) => response.data);
+}
